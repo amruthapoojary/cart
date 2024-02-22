a3486af652cf4c42f95d8350b7edee035378ca46
amruthapoojary <amruthapoojary2001@gmail.com>
2024-02-22 20:04:35 +0530

    shop

---
+++ src/data.js
@@ -0,0 +1,35 @@
+
+import React from 'react';
+
+
+
+const data = {
+    productData:[
+        {
+            id:1,
+            img:"https://www.pngitem.com/pimgs/m/10-104091_formal-shirts-for-men-png-image-man-in.png",
+            title: "Full sleeve white shirt",
+            desc: '',
+            price:46,
+
+        },
+        {
+            id:2,
+            img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdrhfsfZV9HqwoJuANioizGqaAznFvgBzuiw&usqp=CAU",
+            title: "Half sleeve girls t-shirt",
+            desc: '',
+            price:26,
+
+        },
+        {
+            id:2,
+            img:"https://5.imimg.com/data5/SELLER/Default/2023/6/320826386/KT/CJ/LL/106458668/6492c940065f4-1687341376-6492c940065fc-500x500.png",
+            title: "Half sleeve ladies t-shirt",
+            desc: '',
+            price:46,
+
+        },
+    ],
+};
+
+export default data;
